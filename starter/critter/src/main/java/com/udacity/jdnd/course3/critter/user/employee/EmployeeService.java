22004c180078edfa847be4df2ce0a8042e21d41f
mustafa sabry <mustafa.braveheart@gmail.com>
2021-01-11 18:42:07 +0200

    Employee availability implementation

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/employee/EmployeeService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/employee/EmployeeService.java
@@ -4,6 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.time.DayOfWeek;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -25,13 +26,31 @@ public class EmployeeService {
     }
 
     public EmployeeDTO getEmployee(long employeeId) {
-        return modelMapper.map(employeeRepository.findById(employeeId), EmployeeDTO.class);
+        return modelMapper.map(employeeRepository.findById(employeeId).get(), EmployeeDTO.class);
     }
 
     public void setAvailability(long employeeId, Set<DayOfWeek> daysAvailable) {
+        Employee employee = employeeRepository.getOne(employeeId);
+        employee.setDaysAvailable(daysAvailable);
+        employeeRepository.save(employee);
     }
 
-    public List<EmployeeDTO> findEmployeesForService(EmployeeRequestDTO employeeDTO) {
-        return null;
+    public List<EmployeeDTO> findEmployeesForService(EmployeeRequestDTO erDTO) {
+        List<Employee> employees = employeeRepository.findDistinctBySkillsInAndDaysAvailableIs(
+                erDTO.getSkills(), erDTO.getDate().getDayOfWeek());
+
+        // filter those that don't contain all skills
+        return convertToEmployeeDTO(employees, erDTO.getSkills());
+    }
+
+    private List<EmployeeDTO> convertToEmployeeDTO(List<Employee> employees, Set<EmployeeSkill> skills) {
+        List<EmployeeDTO> dtos = new ArrayList<>();
+        for (Employee employee: employees){
+            if(employee.getSkills().containsAll(skills)) {
+                EmployeeDTO employeeDTO = modelMapper.map(employee, EmployeeDTO.class);
+                dtos.add(employeeDTO);
+            }
+        }
+        return dtos;
     }
 }
